b53147d051155b7fe590d5d13f41293d1327867f
Robert Moegenburg <r.moegenburg@t-online.de>
2023-11-04 02:25:34 +0100

    fix

--- Life_Support/Scripts/PlayerController.gd
+++ Life_Support/Scripts/PlayerController.gd
@@ -1,69 +1,71 @@
 extends CharacterBody3D
 
+# Reference  #
+@onready var head = $Head
+
 # Variables  #
-var vel = Vector3()
-const MAX_SPEED = 20
-const ACCEL = 4.5
+var current_speed  = 5.0
 
-var dir = Vector3()
+const walking_speed = 5.0
+const sprinting_speed = 8.0
+const crouching_speed = 3.0
 
-const DEACCEL = 16
-const MAX_SLOPE_ANGLE  = 40
+const jump_velocity = 4.5
 
-var camera
-var rotation_helper
+const mouse_sens = 0.4
 
-var MOUSE_SENSITIVITY =0.05
+var lerp_speed = 10.0
+
+var direction = Vector3.ZERO
+
+var crouching_depth = -0.5
+
+#Get the gravity from the project settings to be synced with Rigidbody nodes
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	camera = $CameraMain
-	rotation_helper = $RotationHelper
-
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
+
 func _physics_process(delta):
-	HandleMovement(delta)
+
+	if Input.is_action_pressed("crouch"):
+		current_speed = crouching_speed
+		head.position.y = 0.595 - crouching_depth
+	else:
+		head.position.y = 0.595
+		if(Input.is_action_pressed("sprint")):
+			current_speed = sprinting_speed
+		else:
+			current_speed = walking_speed
+	
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+
+	#Handle Jump
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = jump_velocity
+
+	var input_dir = Input.get_vector("Left","Right", "Forward", "Backward")
+	# Lerp direction #
+
+	direction = lerp(direction,(transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized(), delta *lerp_speed)
+
+	if direction:
+		velocity.x = direction.x * current_speed
+		velocity.z = direction.z * current_speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, current_speed)
+		velocity.z = move_toward(velocity.z, 0, current_speed)
+	move_and_slide()
 
 func _input(event):
-	if event is InputEventMouseMotion and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
-		# rotation_helper.rotate_x(deg_to_rad(event.relative.y * MOUSE_SENSITIVITY))
-		self.rotate_y(deg_to_rad(event.relative.x * MOUSE_SENSITIVITY * -1))
-		self.rotate_x(deg_to_rad(event.relative.y * MOUSE_SENSITIVITY * -1))
-		var camera_rot = rotation_helper.rotation_degrees
-		camera_rot.x  = clamp(camera_rot.x, -70, 70)
+	if event is InputEventMouseMotion:
+		rotate_y(deg_to_rad(-event.relative.x * mouse_sens))
+		head.rotate_x(deg_to_rad(-event.relative.y * mouse_sens))
+		head.rotation.x = clamp(head.rotation.x, deg_to_rad(-89), deg_to_rad(89))
 		
-		rotation_helper.rotation_degrees = camera_rot
-
-func HandleMovement(delta):
-	dir = Vector3()
-	var cam_xform = camera.get_global_transform()
-	var input_movement_vector = Input.get_vector("Left","Right","Backward", "Forward")
-
-	# Basic vectors are already normalized.
-	dir += -cam_xform.basis.z * input_movement_vector.y
-	dir += cam_xform.basis.x * input_movement_vector.x
-	dir.y =0
-	dir = dir.normalized()
-
-	#Velocity
-	var hvel = vel
-	var target = dir
-	target *= MAX_SPEED
-	var accel
-	if dir.dot(hvel) > 0:
-		accel = ACCEL
-	else:
-		accel = DEACCEL
-	
-	hvel = hvel.lerp(target, accel * delta)
-	velocity.x = hvel.x
-	velocity.z = hvel.z
-	velocity.y = 0
-	move_and_slide()
-	
-	
-
