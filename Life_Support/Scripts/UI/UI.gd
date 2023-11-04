ff82514a4eb1f4c198dc0902b0dd27f9bf1c59ce
Robert Moegenburg <r.moegenburg@t-online.de>
2023-11-04 18:31:34 +0100

    globalUI

--- Life_Support/Scripts/UI/UI.gd
+++ Life_Support/Scripts/UI/UI.gd
@@ -2,10 +2,11 @@ extends Control
 #top of script
 @onready var previous_window = DisplayServer.window_get_mode()
 @onready var current_window = DisplayServer.window_get_mode()
+# MENUES
+@onready var hmenuPanel = $HMenu
+@onready var hsettingPanel = $SettingMenu
+@onready var hcreditPanel = $CreditMenu
 #Vars 
-var scene_setting = load("res://Nodes/UI/Settings.tscn")
-var scene_credits = load("res://Nodes/UI/Credits.tscn")
-
 var GAMEVAR_MUSIC_VOLUME = 0.5
 var GAMEVAR_SOUND_VOLUME = 0.5
 var GAMEVAR_MOUSE_SENSIVITY = 0.5
@@ -14,20 +15,26 @@ var GAMEVAR_ISPAUSED = false
 
 
 func _on_resume_pressed():
-	self.queue_free()
+	ShowNode(self,false)
+	print("RESUME")
 	# TIMESCALE  ?!
 	
 func _on_play_pressed():
-	get_tree().change_scene_to_file("res://Nodes/game_scene.tscn")
+	get_tree().get_tree().change_scene_to_file("res://Nodes/game_scene.tscn")
+	ShowNode(self,false)
+	print("PLAY")
 
 func _on_options_pressed():
-	var instance = scene_setting.instantiate()
-	add_child(instance)
-	self.queue_free()
+	ShowNode(hsettingPanel,true)
+	ShowNode(hcreditPanel,false)
+	print("SETTINGS")
+	
 
 func _on_credits_pressed():
-	var instance = scene_credits.instantiate()
-	add_child(instance)
+	ShowNode(hsettingPanel,false)
+	ShowNode(hcreditPanel,true)
+	print("CREDITS")
+	
 
 
 	
@@ -38,17 +45,20 @@ func _on_quit_pressed():
 		
 func _on_v_slider_3_mouse_s_value_changed(value:float):
 	GAMEVAR_MOUSE_SENSIVITY = 100 / value
-
+	print (GAMEVAR_MOUSE_SENSIVITY)
 
 func _on_v_slider_2_sound_value_changed(value:float):
 	GAMEVAR_SOUND_VOLUME = 100 / value
+	print (GAMEVAR_SOUND_VOLUME)
 	
 func _on_v_slider_music_value_changed(value:float):
 	GAMEVAR_MUSIC_VOLUME = 100 / value
+	print (GAMEVAR_MUSIC_VOLUME)
 
 
 
 func _on_check_box_2_screen_toggled(button_pressed:bool):
+	print("WINDOW-MODE is", button_pressed)
 	current_window = DisplayServer.window_get_mode()
 	if current_window != 4 && button_pressed:
 		previous_window = current_window
@@ -60,3 +70,13 @@ func _on_check_box_2_screen_toggled(button_pressed:bool):
 
 func _on_check_box_invert_toggled(button_pressed:bool):
 	GAMEVAR_INVERT = button_pressed
+	print(GAMEVAR_INVERT)
+
+func ShowNode(node:Node, value: bool) -> void:
+	if !value:
+		node.process_mode = 4 # = Mode: Disabled
+		node.hide()
+	else:
+		node.process_mode = 0 # = Mode: Inherit
+		node.show()
+
