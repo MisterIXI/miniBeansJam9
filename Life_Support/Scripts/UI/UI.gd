66171b8abb52a0b748ea889fc52c26c73c7e98ce
Robert Moegenburg <r.moegenburg@t-online.de>
2023-11-04 19:25:24 +0100

    ooo

--- Life_Support/Scripts/UI/UI.gd
+++ Life_Support/Scripts/UI/UI.gd
@@ -6,6 +6,12 @@ extends Control
 @onready var hmenuPanel = $HMenu
 @onready var hsettingPanel = $SettingMenu
 @onready var hcreditPanel = $CreditMenu
+# BUTTONS AND SLIDERS
+@onready var button_Start : Button  = $HMenu/MarginContainer/VBoxContainer/Button_Start
+@onready var button_Settings : Button  = $HMenu/MarginContainer/VBoxContainer/Button_Settings
+@onready var button_Credits : Button  = $HMenu/MarginContainer/VBoxContainer/Button_Credits
+@onready var button_Quit : Button = $HMenu/MarginContainer/VBoxContainer/Button_Quit
+
 #Vars 
 var GAMEVAR_MUSIC_VOLUME = 0.5
 var GAMEVAR_SOUND_VOLUME = 0.5
@@ -13,6 +19,12 @@ var GAMEVAR_MOUSE_SENSIVITY = 0.5
 var GAMEVAR_INVERT = false
 var GAMEVAR_ISPAUSED = false
 
+
+func _ready():
+	button_Start.connect("pressed", self._on_play_pressed())
+	button_Settings.connect("pressed",self._on_options_pressed())
+	button_Credits.connect("pressed", self._on_credits_pressed()) 
+	button_Quit.connect("pressed", self._on_quit_pressed())
 
 func _on_resume_pressed():
 	ShowNode(self,false)
